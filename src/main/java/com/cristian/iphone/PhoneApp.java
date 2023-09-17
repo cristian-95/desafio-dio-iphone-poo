48a5fed84d301cd61091f99de6f35b5a97fd57ec
cristian <nem.cristianrc@gmail.com>
2023-09-17 19:34:35 -0300

    feat: add PhoneApp class

---
+++ src/main/java/com/cristian/iphone/PhoneApp.java
@@ -0,0 +1,13 @@
+package com.cristian.iphone;
+
+public class PhoneApp implements Screen{
+    @Override
+    public void showIcons() {
+
+    }
+
+    @Override
+    public void showOptions() {
+
+    }
+}
